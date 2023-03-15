01e76290e9b9b8eb7a701fbc198e437a971a64a1
NicolasRidoire <nridoire@gmail.com>
2023-03-15 13:52:19 +0100

    home page

--- src/templates_c/716e12a4afb95e681660c345a77d119053be5a4e_0.file.home.tpl.php
+++ src/templates_c/716e12a4afb95e681660c345a77d119053be5a4e_0.file.home.tpl.php
@@ -1,9 +1,68 @@
-{extends file="navbar.tpl"}
-{block name="include"}
-    <link rel="stylesheet" href="./home/home.css">
-{/block}
+<?php
+/* Smarty version 4.2.1, created on 2023-03-15 13:46:11
+  from 'C:\www\Projet-Web-G3\src\templates\home.tpl' */
 
-{block name=content}
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.1',
+  'unifunc' => 'content_6411be13377d33_42716138',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '716e12a4afb95e681660c345a77d119053be5a4e' => 
+    array (
+      0 => 'C:\\www\\Projet-Web-G3\\src\\templates\\home.tpl',
+      1 => 1678884314,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6411be13377d33_42716138 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20015540586411be13375609_89322138', "include");
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1815820766411be13377509_94206288', 'content');
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, "navbar.tpl");
+}
+/* {block "include"} */
+class Block_20015540586411be13375609_89322138 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'include' => 
+  array (
+    0 => 'Block_20015540586411be13375609_89322138',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+    <link rel="stylesheet" href="./home/home.css">
+<?php
+}
+}
+/* {/block "include"} */
+/* {block 'content'} */
+class Block_1815820766411be13377509_94206288 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_1815820766411be13377509_94206288',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
     <div>
         <section class="carousel" aria-label="Gallery">
             <ol class="carousel__viewport">
@@ -47,4 +106,8 @@
             </aside>
         </section>
     </div>
-{/block}
+<?php
+}
+}
+/* {/block 'content'} */
+}
