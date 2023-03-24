6fa5aeee3ede614bb3a583f46e05c61424586e64
LAPTOP-VMULRDRU\maxen <maxence.valette@hotmail.com>
2023-03-24 13:52:43 +0100

    JS sur connexion

--- src/templates_c/713c1ee1f47e49703cdd4732e12204032e790bc2_0.file.connexion.tpl.php
+++ src/templates_c/713c1ee1f47e49703cdd4732e12204032e790bc2_0.file.connexion.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2023-03-22 13:55:54
+/* Smarty version 4.2.1, created on 2023-03-24 12:00:45
   from 'C:\www\ProjetMobile\new\Projet-Web-G3\src\templates\connexion.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_641afada012fd7_83728432',
+  'unifunc' => 'content_641d82ddc07ec1_93041722',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '713c1ee1f47e49703cdd4732e12204032e790bc2' => 
     array (
       0 => 'C:\\www\\ProjetMobile\\new\\Projet-Web-G3\\src\\templates\\connexion.tpl',
-      1 => 1679475748,
+      1 => 1679655643,
       2 => 'file',
     ),
   ),
@@ -20,29 +20,27 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_641afada012fd7_83728432 (Smarty_Internal_Template $_smarty_tpl) {
+function content_641d82ddc07ec1_93041722 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1047142603641afada011de1_80053756', "navbarcss");
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1877926075641d82ddc06fa6_38819875', "navbarcss");
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1340270928641afada012b68_58935672', 'content');
-?>
-
-<?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, 'footer.tpl');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1338731028641d82ddc076f9_09576004', 'content');
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, 'footer.tpl');
 }
 /* {block "navbarcss"} */
-class Block_1047142603641afada011de1_80053756 extends Smarty_Internal_Block
+class Block_1877926075641d82ddc06fa6_38819875 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'navbarcss' => 
   array (
-    0 => 'Block_1047142603641afada011de1_80053756',
+    0 => 'Block_1877926075641d82ddc06fa6_38819875',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -54,28 +52,71 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 }
 /* {/block "navbarcss"} */
 /* {block 'content'} */
-class Block_1340270928641afada012b68_58935672 extends Smarty_Internal_Block
+class Block_1338731028641d82ddc076f9_09576004 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_1340270928641afada012b68_58935672',
+    0 => 'Block_1338731028641d82ddc076f9_09576004',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
+    <?php echo '<script'; ?>
+>
+        function validateForm() {
+            var mail = document.forms["formulaire"]["mail"];
+            var mdp = document.forms["formulaire"]["mdp"]
+            var bool = false;
+
+            if (mail.value == "") {
+                document.getElementById('errormail').innerHTML = "* Veuillez entrez un mail valide!";
+                mail.focus();
+                bool = false;
+            } else {
+                document.getElementById('errormail').innerHTML = "";
+            }
+            if (mdp.value == "") {
+                document.getElementById('errorpass').innerHTML = "* Veuillez entrez un Mot de passe valide!";
+                mdp.focus();
+                bool = false;
+            } else {
+                document.getElementById('errorpass').innerHTML = "";
+            }
+            return bool;
+        }
+    <?php echo '</script'; ?>
+>
+    <style>
+        .error {
+            color: #D8000C;
+            margin-left : 20%;
+            padding-top:5%;
+            
+        }
+        @media only screen and (max-width: 830px)
+        {
+            .error{
+                margin-left:0%;
+            }
+        }
+    </style>
+
     <div class="allh">
         <div class="wrapper">
             <div class="form">
-                <form action="index.php" method="post">
+                <form name="formulaire" action="index.php" method="post" id="form" onSubmit="return validateForm()">
                     <input class="champ" id="email" type="email" name="mail" placeholder="Adresse e-mail">
+                    <span class="error" id="errormail"></span>
                     <input class="champ" id="password" type="password" name="mdp" placeholder="Mot de passe">
+                    <span class="error" id="errorpass"></span> 
                     <button class="champ" id="connect" type="submit" name="p" value="home">Se connecter</button>
                 </form>
             </div>
         </div>
     </div>
+
 <?php
 }
 }
